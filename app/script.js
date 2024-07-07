20441230f422a1be1a68df43649e26d5f978ba6d
zmytro <1223334444987kdarvis@gmail.com>
2024-07-07 16:17:01 -0400

    logic rebuild and beautify

--- app/script.js
+++ app/script.js
@@ -1,123 +1,127 @@
 var targetDate = new Date("July 14, 2024 20:00:00").getTime();
 
-const loadStartTime = new Date().getTime();
+window.addEventListener("DOMContentLoaded", function () {
 
-var timer = setInterval(function () {
+    const loadStartTime = new Date().getTime();
 
-    var currentDate = new Date().getTime();
+    var timer = setInterval(function () {
 
-    var remainingTime = targetDate - currentDate;
+        var currentDate = new Date().getTime();
 
-    // Переведите миллисекунды в дни, часы, минуты и секунды
-    var days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+        var remainingTime = targetDate - currentDate;
 
-    //console.log(days + "days " + hours + "hours " + minutes + "minutes " + seconds + "seconds ");
+        // Переведите миллисекунды в дни, часы, минуты и секунды
+        var days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
 
-    if (days < 10) {
-        days = "0" + days;
-    }
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-    if (seconds < 10) {
-        seconds = "0" + seconds;
-    }
-    // Если время истекло
-    if (remainingTime < 0) {
-        clearInterval(timer);
-        var days = '00';
-        var hours = '00';
-        var minutes = '00';
-        var seconds = '00';
-        console.log("Время истекло!");
-    }
+        //console.log(days + "days " + hours + "hours " + minutes + "minutes " + seconds + "seconds ");
 
-    var timerElementDays = document.getElementById("timerDays-p");
-    var timerElementHours = document.getElementById("timerHours-p");
-    var timerElementMinutes = document.getElementById("timerMinutes-p");
-    var timerElementSeconds = document.getElementById("timerSeconds-p");
-
-    timerElementDays.innerHTML = days;
-    timerElementHours.innerHTML = hours ;
-    timerElementMinutes.innerHTML = minutes ;
-    timerElementSeconds.innerHTML = seconds;
-
-}, 1000);
-
-
-
-
-window.addEventListener("load", function() {
-    const loadEndTime = new Date().getTime();
-    const loadTime = loadEndTime - loadStartTime;
-
-    // Проверяем, было ли это первое посещение
-    const isFirstVisit = !sessionStorage.getItem('visited');
-    
-    // Если это первое посещение, добавляем запись в sessionStorage
-    if (isFirstVisit) {
-        sessionStorage.setItem('visited', 'true');
-    }
-
-    // Устанавливаем задержку только если это первое посещение и время загрузки меньше 3 секунд
-    const delay = (isFirstVisit && loadTime < 3000) ? 1500 : 0;
-
-    console.log("Загрузка страницы заняла: " + loadTime + " мс");
-    console.log(delay);
-
-    setTimeout(function() {
-
-     // Добавляем класс для скрытия загрузочного экрана
-     document.getElementById('loader').classList.add('hidden');
-    
-     // Делаем паузу, чтобы переход завершился, и затем скрываем элемент из DOM
-     setTimeout(function() {
-         // Скрываем загрузочный экран из DOM
-         document.getElementById('loader').style.display = 'none';
-         // Показываем основной контент
-         document.getElementById('home-container').style.display = 'flex';
- 
-
-    const blocks = document.querySelectorAll('.fade-in');
-
-    function fadeInBlocks(index) {
-        if (index < blocks.length) {
-            setTimeout(function() {
-                blocks[index].classList.add('active');
-                fadeInBlocks(index + 1);
-            }, 500); 
+        if (days < 10) {
+            days = "0" + days;
         }
-    }
-
-    fadeInBlocks(0);
-    console.log('fadeInBlocks');
-
-
-      
-        let currentIndex = 0;
-        const slides = document.querySelectorAll('.carousel img');
-        const totalSlides = slides.length;
-        const wrapper = document.querySelector('.carousel-wrapper');
-
-        function showSlide(index) {
-            wrapper.style.transform = `translateX(-${index * 100}%)`;
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        // Если время истекло
+        if (remainingTime < 0) {
+            clearInterval(timer);
+            var days = '00';
+            var hours = '00';
+            var minutes = '00';
+            var seconds = '00';
+            console.log("Время истекло!");
         }
 
-        function nextSlide() {
-            currentIndex = (currentIndex + 1) % totalSlides;
-            showSlide(currentIndex);
+        var timerElementDays = document.getElementById("timerDays-p");
+        var timerElementHours = document.getElementById("timerHours-p");
+        var timerElementMinutes = document.getElementById("timerMinutes-p");
+        var timerElementSeconds = document.getElementById("timerSeconds-p");
+
+        timerElementDays.innerHTML = days;
+        timerElementHours.innerHTML = hours;
+        timerElementMinutes.innerHTML = minutes;
+        timerElementSeconds.innerHTML = seconds;
+
+    }, 1000);
+
+
+
+
+    window.addEventListener("load", function () {
+
+        const loadEndTime = new Date().getTime();
+        const loadTime = loadEndTime - loadStartTime;
+
+        // Проверяем, было ли это первое посещение
+        const isFirstVisit = !sessionStorage.getItem('visited');
+
+        // Если это первое посещение, добавляем запись в sessionStorage
+        if (isFirstVisit) {
+            sessionStorage.setItem('visited', 'true');
         }
 
-        setInterval(nextSlide, 3000); // Change image every 3 seconds
+        // Устанавливаем задержку только если это первое посещение и время загрузки меньше 3 секунд
+        const delay = (isFirstVisit && loadTime < 3000) ? 1500 : 0;
 
-}, 0); // Время задержки должно совпадать с временем transition
-}, delay);
+        console.log("Загрузка страницы заняла: " + loadTime + " мс");
+        console.log(delay);
+
+        setTimeout(function () {
+
+            // Добавляем класс для скрытия загрузочного экрана
+            document.getElementById('loader').classList.add('hidden');
+
+            // Делаем паузу, чтобы переход завершился, и затем скрываем элемент из DOM
+            setTimeout(function () {
+                // Скрываем загрузочный экран из DOM
+                document.getElementById('loader').style.display = 'none';
+                // Показываем основной контент
+                document.getElementById('home-container').style.display = 'flex';
+
+
+                const blocks = document.querySelectorAll('.fade-in');
+
+                function fadeInBlocks(index) {
+                    if (index < blocks.length) {
+                        setTimeout(function () {
+                            blocks[index].classList.add('active');
+                            fadeInBlocks(index + 1);
+                        }, 500);
+                    }
+                }
+
+                fadeInBlocks(0);
+                console.log('fadeInBlocks');
+
+
+
+                let currentIndex = 0;
+                const slides = document.querySelectorAll('.carousel img');
+                const totalSlides = slides.length;
+                const wrapper = document.querySelector('.carousel-wrapper');
+
+                function showSlide(index) {
+                    wrapper.style.transform = `translateX(-${index * 100}%)`;
+                }
+
+                function nextSlide() {
+                    currentIndex = (currentIndex + 1) % totalSlides;
+                    showSlide(currentIndex);
+                }
+
+                setInterval(nextSlide, 3000); // Change image every 3 seconds
+
+            }, 0); // Время задержки должно совпадать с временем transition
+        }, delay);
+    });
 });
 
 
